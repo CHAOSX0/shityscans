f417fd30b5e12e407d521bd7ca6cb371bdb845b3
CHAOSX0 <86735864+CHAOSX0@users.noreply.github.com>
2023-08-29 20:50:21 +0300

    remove site folder

--- site/src/lib/supabase.js
+++
@@ -1,5 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient('https://rathqhufdejjehkdxfuy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhdGhxaHVmZGVqamVoa2R4ZnV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MDk4ODIsImV4cCI6MjAwNjM4NTg4Mn0.AnTJfrt8mgtwdg2WTxaMSRrdCmgAgphpvub78irJRKA')
