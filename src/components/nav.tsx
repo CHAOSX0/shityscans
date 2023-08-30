f1d9f818503345c56bd044ccc326cd64b223e9de
ghost <saifalwaqfi45@gmail.com>
2023-08-30 19:21:29 +0300

    Made the changing in the nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -21,20 +21,22 @@ export default function Nav({Items}: {Items: Array<{text: string, URL: string}>}
     return (
         <div className="th">
 <div className="centernav bound">
-  <div className="shme">
-    <i className="fa fa-bars" aria-hidden="true" />
+  <div className="hidden">
+    <i className="fa fa-bars font-black inline-block not-italic leading-none" aria-hidden="true" />
   </div>
-  <header role="banner" >
+  <header role="banner" className="bannerClassName float-right" >
     <div className="site-branding logox">
-      <span className="logos">
+      <span className="logos m-0 block">
         <a
           title="مانجا ARESManga | أفضل موقع للمانهوا والمانجا العربية - افضل موقع للمانجا المترجمة يضم مكتبة هائلة من المانجا المترجمة يمتاز بسرعة ترجمة الفصول الجديدة."
           itemProp="url"
           href="https://aresmanga.org/"
+          className="logosA text-white no-underline"
         >
           <img
             src="https://aresmanga.org/wp-content/uploads/2022/12/ares-logo-blank.png"
             alt="مانجا ARESManga | أفضل موقع للمانهوا والمانجا العربية - افضل موقع للمانجا المترجمة يضم مكتبة هائلة من المانجا المترجمة يمتاز بسرعة ترجمة الفصول الجديدة."
+            className="block align-middle max-w-full h-auto"
           />
           <span className="hdl">
             مانجا ARESManga | أفضل موقع للمانهوا والمانجا العربية
@@ -47,18 +49,18 @@ export default function Nav({Items}: {Items: Array<{text: string, URL: string}>}
       />
     </div>
   </header>
-  <nav id="main-menu" className="mm">
+  <nav id="main-menu" className="mm float-right">
     <span
       itemType="http://schema.org/SiteNavigationElement"
       role="navigation"
     >
-      <ul id="menu-menu" className="menu">
+      <ul id="menu-menu" className="menu relative float-left p-0 m-0">
         {navELements}
       </ul>{" "}
     </span>
-    <div className="clear" />
+    <div className="clear-both" />
   </nav>
-  <div className="searchx minmb">
+  <div className="searchx minmb relative float-left">
     <form
       action="https://aresmanga.org/"
       id="form"
@@ -66,12 +68,13 @@ export default function Nav({Items}: {Items: Array<{text: string, URL: string}>}
       itemProp="potentialAction"
 
       itemType="http://schema.org/SearchAction"
+      className="p-0 relative"
     >
       <meta itemProp="target" content="https://aresmanga.org/?s={query}" />
       <input
         id="s"
         itemProp="query-input"
-        className="search-live"
+        className="search-live pl-8 pr-3 font-light bg-gray-900 block w-full h-8 text-sm text-white"
         type="text"
         placeholder="بحث"
         name="s"
@@ -85,11 +88,11 @@ export default function Nav({Items}: {Items: Array<{text: string, URL: string}>}
       </div>
     </form>
   </div>
-  <div className="srcmob">
+  <div className="srcmob hidden">
     <i className="fas fa-search" aria-hidden="true" />
   </div>
 </div>
-<div className="clear" />
+<div className="clear-both" />
 </div>
     )
 }
