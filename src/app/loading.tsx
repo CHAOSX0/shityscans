d6a37f514a573822e2c5369a5b25e2698478e06b
CHAOSX0 <86735864+CHAOSX0@users.noreply.github.com>
2023-09-20 14:11:25 +0300

    loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import './loading.css'
+
+export default function (){
+    return (
+        <div>
+            <section className="spinner-block">
+  
+  <section className="comet-spinner" tabIndex={1} aria-label="comet spinner">
+    <div className="inner-circle"></div>
+    <div className="comet"></div>
+  </section>
+  
+  </section>
+        </div>
+    )
+}
