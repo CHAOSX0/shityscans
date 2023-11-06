d37cc35231010c7082236ada5346e2da18261f51
CHAOSX0 <86735864+CHAOSX0@users.noreply.github.com>
2023-11-06 12:27:55 +0300

    remove say hi

--- src/app/chapter/[chapterID]/hi.tsx
+++ src/app/chapter/[chapterID]/hi.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
-import { toast } from "react-hot-toast";
 
 export default function SayHi({number}: {number: number}){
-    useEffect(()=>{
-      toast.success(`موسى يقول: تبا لك يا محمود وصلت الى الفصل ${number}`)
-    },[])
+    
 return (
     <>
     </>
